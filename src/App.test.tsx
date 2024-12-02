747b58b52a2aec2c074e37876e3816633f8c3359
Toxinlab <toxinlab@gmail.com>
2024-12-03 00:22:05 +0100

    init dev commit

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('tdd support', () => {
-  const fake = true;
-  expect(fake).toBe(true)
-});
